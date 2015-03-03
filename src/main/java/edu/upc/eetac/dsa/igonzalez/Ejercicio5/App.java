78660c8954f0ffe261be001a5299053f11ca7f98
Ivan <Ivan@10.189.164.107>
2015-03-03 14:37:55 +0100

    Ejercicio5

---
+++ src/main/java/edu/upc/eetac/dsa/igonzalez/Ejercicio5/App.java
@@ -0,0 +1,13 @@
+package edu.upc.eetac.dsa.igonzalez.Ejercicio5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
